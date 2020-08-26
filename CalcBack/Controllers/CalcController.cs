9f49066f78a9127a635961605098ca3d5c1a31b6
Nemyr <antoshko@list.ru>
2020-08-26 17:45:35 +0300

    Создание проекта + Контроллер для калькулятора

---
+++ CalcBack/Controllers/CalcController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalcBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+
+        //[HttpPost("{param1}/{param2}/{act}")]
+        [HttpGet("[action]/{param1}/{param2}/{act}")]
+        public ActionResult<float> Count(float param1, float param2, string act)
+        {
+            
+            
+            return 123f;
+        }
+
+    }
+}
