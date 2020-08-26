2375f1da5a93e44dc2b901aa890bc87e54c909d1
Nemyr <antoshko@list.ru>
2020-08-26 18:04:02 +0300

    Логика запроса

--- CalcBack/Controllers/CalcController.cs
+++ CalcBack/Controllers/CalcController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CalcBack.Classes;
+using CalcBack.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,9 +18,11 @@ namespace CalcBack.Controllers
         [HttpGet("[action]/{param1}/{param2}/{act}")]
         public ActionResult<float> Count(float param1, float param2, string act)
         {
+
+            ICounterFactory cFactory = new CounterFactory();
+            ICounter counter = cFactory.GetCounter(act);
             
-            
-            return 123f;
+            return counter.Count(param1, param2);
         }
 
     }
---
+++ CalcBack/Interfaces/ICounterFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalcBack.Interfaces
+{
+    public interface ICounterFactory
+    {
+        ICounter GetCounter(string type);
+    }
+}
